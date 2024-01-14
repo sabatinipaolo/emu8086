a50d568360d7e01c6adc123ac24d8a30e584ae1f
sabatinipaolo <113015247+sabatinipaolo@users.noreply.github.com>
2024-01-14 12:57:34 +0100

    Add files via upload

---
+++ 200_strcpy.asm
@@ -0,0 +1,46 @@
+; multi-segment executable file template.
+
+data segment
+    ; add your data here!
+    stringa_sorgente     db "sono la stringa",0
+    stringa_destinazione db "             ",0
+    
+ends
+
+
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+
+
+; add your code here
+            
+    lea si, stringa_sorgente 
+    lea di, stringa_destinazione
+    
+    
+
+while:
+    mov ah, [si]
+    cmp ah , 0 
+    je fine_while
+        
+        mov [di], ah
+        inc si
+        inc di 
+    
+    jmp while
+        
+         
+fine_while:
+
+    hlt
+    
+        
+ends
+
+end start ; set entry point and stop the assembler.
